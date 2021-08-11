5dbde34678939d47320a2053c966d6865eae7c62
Exequiel Muñoz <eamunooz@gmail.com>
2021-08-11 11:43:10 -0400

    Watch List 3

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :list
-  belongs_to :movie, dependent: :destroy
+  belongs_to :movie
+
+  validates :movie_id, uniqueness: { scope: :list_id, message: "problema!" }
+  validates :comment, length: { minimum: 6 }
 end
